626fb69ed5c124d326d2e566b8546563c2b0e1a9
abheesheksharma <koolabhishek837@gmail.com>
2018-03-28 19:15:12 +0530

    Update MainActivity.java

--- android/ShowWebsiteInAndroidApp/MainActivity.java
+++ android/ShowWebsiteInAndroidApp/MainActivity.java
@@ -4,5 +4,17 @@ private WebView myWebView;
 // within oncreate function
 myWebView = (WebView) findViewById(R.id.webView);
 WebSettings webSettings = myWebView.getSettings();
-
 webSettings.setJavaScriptEnabled( true ); // It enabls to eun javascript in app
+myWebView.setWebViewClient(new WebViewClient()); // It for open other links of website inside the app
+
+//This is new function for backbutton
+@Override
+    public void onBackPressed() {
+        if(myWebView.canGoBack()){
+            myWebView.goBack();
+        }
+        else
+        {
+            super.onBackPressed();
+        }
+    }
